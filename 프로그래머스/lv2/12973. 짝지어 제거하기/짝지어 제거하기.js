946957ec37acbc0b7f6252c66114802757bdd221
노민섭 <107898015+ssups@users.noreply.github.com>
2023-04-05 15:59:08 +0900

    [level 2] Title: 짝지어 제거하기, Time: 59.09 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,7 +2,7 @@ function solution(string){
     const stack = [];
     for(const each of string){
         stack.push(each);
-        while(stack.length >= 2 && stack[stack.length - 1] === stack[stack.length - 2]){
+        if(stack.length >= 2 && stack[stack.length - 1] === stack[stack.length - 2]){
             stack.splice(-2);
         } 
     }
