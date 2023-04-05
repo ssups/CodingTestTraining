9a6681483693059d472a417e5e717de09e4768b6
노민섭 <107898015+ssups@users.noreply.github.com>
2023-04-05 15:35:45 +0900

    [level 2] Title: 피보나치 수, Time: 4.43 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,8 +1,8 @@
 function solution(n) {
     const fibonachi = [0 ,1];
     for(let i = 2; i <= n ; i++){
-        const next = BigInt(fibonachi[i-1]) + BigInt(fibonachi[i-2]);
+        const next = ((fibonachi[i-1]) + (fibonachi[i-2]))% 1234567;
         fibonachi.push(next);
     }
-    return fibonachi[n] % 1234567n;
+    return fibonachi[n] ;
 }
